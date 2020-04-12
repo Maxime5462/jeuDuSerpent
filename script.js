c68657103d74a972330d429dbaf14b65dfca1925
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2020-04-12 10:36:50 +0200

    Diriger le serpent

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.onload = function () {
     canvas.style.border = '1px solid'
     document.body.appendChild(canvas)
     ctx = canvas.getContext('2d')
-    snakee = new Snake([[6, 4], [5, 4], [4, 4]])
+    snakee = new Snake([[6, 4], [5, 4], [4, 4]], 'right')
     refreshCanvas()
   }
 
@@ -31,8 +31,9 @@ window.onload = function () {
     ctx.fillRect(x, y, blockSize, blockSize)
   }
 
-  function Snake (body) {
+  function Snake (body, direction) {
     this.body = body
+    this.direction = direction
     this.draw = function () {
       ctx.save()
       ctx.fillStyle = '#ff0000'
@@ -43,9 +44,69 @@ window.onload = function () {
     }
     this.advance = function () {
       var nextPosition = this.body[0].slice()
-      nextPosition[0]++
+      switch (this.direction) {
+        case 'left':
+          nextPosition[0] -= 1
+          break
+        case 'right':
+          nextPosition[0] += 1
+          break
+        case 'down':
+          nextPosition[1] += 1
+          break
+        case 'up':
+          nextPosition[1] -= 1
+          break
+        default:
+          throw ('Invalid Direction')
+      }
       this.body.unshift(nextPosition)
       this.body.pop()
     }
+    this.setDirection = function (newDirection) {
+      var allowedDirections
+      console.log(this)
+      switch (this.direction) {
+        case 'left':
+          allowedDirections = ['up', 'down']
+          break
+        case 'right':
+          allowedDirections = ['up', 'down']
+          break
+        case 'down':
+          allowedDirections = ['left', 'right']
+          break
+        case 'up':
+          allowedDirections = ['left', 'right']
+          break
+        default:
+          throw ('Invalid Direction')
+      }
+      if (allowedDirections.indexOf(newDirection) > -1) {
+        this.direction = newDirection
+      }
+    }
+  }
+  document.onkeydown = function handleKeyDOwn (e) {
+    var key = e.keyCode
+    var newDirection
+    switch (key) {
+      case 37:
+        newDirection = 'left'
+        break
+      case 38:
+        newDirection = 'up'
+        break
+      case 39:
+        newDirection = 'right'
+        break
+      case 40:
+        newDirection = 'down'
+        break
+      default:
+        return
+    }
+
+    snakee.setDirection(newDirection)
   }
 }
