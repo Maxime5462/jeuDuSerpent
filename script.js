ba860b3de8ffb29dc247e5d5f2cdf09d1dfbe22c
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2020-04-13 17:08:45 +0200

    Ajouter la pomme

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.onload = function () {
   var ctx
   var delay = 100
   var snakee
+  var applee
 
   init()
 
@@ -16,6 +17,7 @@ window.onload = function () {
     document.body.appendChild(canvas)
     ctx = canvas.getContext('2d')
     snakee = new Snake([[6, 4], [5, 4], [4, 4]], 'right')
+    applee = new Apple([10, 10])
     refreshCanvas()
   }
 
@@ -23,6 +25,7 @@ window.onload = function () {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight)
     snakee.advance()
     snakee.draw()
+    applee.draw()
     setTimeout(refreshCanvas, delay)
   }
   function drawBlock (ctx, position) {
@@ -87,6 +90,22 @@ window.onload = function () {
       }
     }
   }
+
+  function Apple (position) {
+    this.position = position
+    this.draw = function () {
+      ctx.save()
+      ctx.fillStyle = '#33cc33'
+      ctx.beginPath()
+      var radius = blockSize / 2
+      var x = position[0] * blockSize + radius
+      var y = position[1] * blockSize + radius
+      ctx.arc(x, y, radius, 0, Math.PI * 2, true)
+      ctx.fill()
+      ctx.restore()
+    }
+  }
+
   document.onkeydown = function handleKeyDown (e) {
     var key = e.keyCode
     var newDirection
@@ -108,6 +127,5 @@ window.onload = function () {
     }
 
     snakee.setDirection(newDirection)
-            
   }
 }
