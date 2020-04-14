438cbff1c804923af036d8fee6740661fe0f4ed6
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2020-04-14 15:23:37 +0200

    Gestion du Score et du Game Over

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ window.onload = function () {
   var applee
   var widthInBlocks = canvasWidth / blockSize
   var heightInBlocks = canvasHeight / blockSize
+  var score
 
   init()
 
@@ -20,6 +21,7 @@ window.onload = function () {
     ctx = canvas.getContext('2d')
     snakee = new Snake([[6, 4], [5, 4], [4, 4]], 'right')
     applee = new Apple([10, 10])
+    score = 0
     refreshCanvas()
   }
 
@@ -27,9 +29,10 @@ window.onload = function () {
     snakee.advance()
 
     if (snakee.checkCollision()) {
-      // Game Over
+      gameOver()
     } else {
       if (snakee.isEatingApple(applee)) {
+        score++
         snakee.ateApple = true
         do { applee.setNewPosition() }
         while (applee.isOnSnake(snakee))
@@ -37,13 +40,37 @@ window.onload = function () {
       ctx.clearRect(0, 0, canvasWidth, canvasHeight)
       snakee.draw()
       applee.draw()
+      drawScore()
       setTimeout(refreshCanvas, delay)
     }
   }
+
+  function gameOver () {
+    ctx.save()
+
+    ctx.fillText('Game Over', 5, 15)
+    ctx.fillText('Appuyer sur la touche Espace pour rejouer', 5, 30)
+
+    ctx.restore()
+  }
+
+  function restart () {
+    snakee = new Snake([[6, 4], [5, 4], [4, 4]], 'right')
+    applee = new Apple([10, 10])
+    score = 0
+    refreshCanvas()
+  }
+
   function drawBlock (ctx, position) {
     var x = position[0] * blockSize
     var y = position[1] * blockSize
     ctx.fillRect(x, y, blockSize, blockSize)
+  }
+
+  function drawScore () {
+    ctx.save()
+    ctx.fillText(score.toString(), 5, canvasHeight - 5)
+    ctx.restore()
   }
 
   function Snake (body, direction) {
@@ -182,6 +209,9 @@ window.onload = function () {
       case 40:
         newDirection = 'down'
         break
+      case 32:
+        restart()
+        return
       default:
         return
     }
